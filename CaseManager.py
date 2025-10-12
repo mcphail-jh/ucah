d958f9e5b70916f8ac04dd21902936a0204e9c2f
mcphail-jh <133837746+mcphail-jh@users.noreply.github.com>
2025-10-11 22:49:44 -0400

    Sampling now stores parameter data for each sample in JSON files. Initialized CaseManager for OneDrive sync.

---
+++ CaseManager.py
@@ -0,0 +1,4 @@
+"""
+This is a class to sync generated CFD cases (IGS files) with the OneDrive cloud storage
+Given a path to a folder of IGS files and corresponding json config files, it will create/update corresponding folders in the team OneDrive folder
+"""
