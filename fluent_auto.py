6ae59f58211bfd63bc527c59572161be655309a1
Soren Poole <qvu2hx@virgina.edu>
2025-10-16 11:04:49 -0400

    fixing fluent closing problem

--- fluent_auto.py
+++ fluent_auto.py
@@ -1,14 +1,19 @@
 import ansys.fluent.core as pyfluent
-from ansys.fluent.core import examples
 
-import_file_name = examples.download_file('mixing_elbow.pmdb', 'pyfluent/mixing_elbow')
-print(f"Importing geometry from: {import_file_name}")
-"""
+import_file_name =  r"C:\Users\Soren Poole\OneDrive - University of Virginia\Capstone\Winged_Missile_2024.IGS"
+
+
 meshing_session = pyfluent.launch_fluent(
-    mode=pyfluent.FluentMode.MESHING, precision=pyfluent.Precision.DOUBLE, processor_count=2
+    show_gui = True, mode=pyfluent.FluentMode.MESHING, precision=pyfluent.Precision.DOUBLE, processor_count=2
 )
+
 watertight = meshing_session.watertight()
 watertight.import_geometry.file_name.set_state(import_file_name)
 watertight.import_geometry.length_unit.set_state('in')
 watertight.import_geometry()
-"""
+
+# stops fluent from closing immediately
+input()
+
+
+
