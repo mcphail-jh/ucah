fbd69f8d12440578fbef1a5bfaa43a879fcfb46d
Soren Poole <qvu2hx@virgina.edu>
2025-10-29 11:48:34 -0400

    adding boundary v2

--- solver_setup.py
+++ solver_setup.py
@@ -24,4 +24,30 @@ viscous.k_omega_model = "sst"
 air = solver_session.setup.materials.fluid["air"]
 
 air.density.option = "ideal-gas"
-input()
+
+# Boundary conditions for inlet - pressure far field
+# gauge pressure : 0 [Pa]
+# mach number : 0.8395
+# temperature : 255.56 [K]
+# x-component of flow direction : 0.998574
+# z-component of flow direction : 0.053382
+# turbulent intensity : 5 [%]
+# turbulent viscosity ratio : 10
+
+
+
+
+pressure_farfield = solver_session.setup.boundary_conditions.pressure_far_field[
+    "inlet"
+]
+pressure_farfield.momentum.gauge_pressure = 100
+pressure_farfield.momentum.mach_number = 5
+pressure_farfield.thermal.temperature = 288.15
+pressure_farfield.momentum.flow_direction[0] = .996194698092
+pressure_farfield.momentum.flow_direction[2] = 0.0871557427477
+pressure_farfield.turbulence.turbulent_intensity = 0.05
+pressure_farfield.turbulence.turbulent_viscosity_ratio = 10
+
+pressure_outlet = solver_session.setup.boundary_conditions.pressure_outlet["outlet"]
+
+pressure_outlet.momentum.backflow_gauge_pressure =
