6c5fffc957cd4a746af04a48ab07dffb1f5f6440
Soren Poole <qvu2hx@virgina.edu>
2025-10-29 11:05:51 -0400

    added path

--- solver_setup.py
+++ solver_setup.py
@@ -1,5 +1,6 @@
 import ansys.fluent.core as pyfluent
+import os
 
-solver = pyfluent.launch_fluent(mode="solver",ui_mode=pyfluent.UIMode.GUI, processor_count=8)
-
-solver.tui.file.read_mesh(path=r"C:\Users\qvu2hx\ucah\first test\Winged_Missile_2024.msh.h5")
+wd = os.path.join(os.getcwd(),"first test")
+solver = pyfluent.launch_fluent(mode="solver",ui_mode=pyfluent.UIMode.GUI, processor_count=5,cwd=wd)
+solver.tui.file.read_mesh(path=os.path.join(wd,"Winged_Missile_2024.msh.h5"))
