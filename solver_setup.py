526c42a387bb39e3c7a115fa78150f7380a79909
Soren Poole <qvu2hx@virgina.edu>
2025-10-29 11:45:46 -0400

    adding boundary

--- solver_setup.py
+++ solver_setup.py
@@ -2,5 +2,26 @@ import ansys.fluent.core as pyfluent
 import os
 
 wd = os.path.join(os.getcwd(),"first test")
-solver = pyfluent.launch_fluent(mode="solver",ui_mode=pyfluent.UIMode.GUI, processor_count=5,cwd=wd)
-solver.tui.file.read_mesh(path=os.path.join(wd,"Winged_Missile_2024.msh.h5"))
+solver_session = pyfluent.launch_fluent(mode="solver",ui_mode=pyfluent.UIMode.GUI, processor_count=5,cwd=wd)
+mesh_path = os.path.join(wd,"Winged_Missile_2024.msh.h5")
+solver_session.file.read_case(file_name = mesh_path)
+
+# model : k-omega
+# k-omega model : sst
+
+viscous = solver_session.setup.models.viscous
+
+viscous.model = "k-omega"
+viscous.k_omega_model = "sst"
+
+# density : ideal-gas
+# viscosity : sutherland
+# viscosity method : three-coefficient-method
+# reference viscosity : 1.716e-05 [kg/(m s)]
+# reference temperature : 273.11 [K]
+# effective temperature : 110.56 [K]
+
+air = solver_session.setup.materials.fluid["air"]
+
+air.density.option = "ideal-gas"
+input()
