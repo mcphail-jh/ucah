0c9fb09e2023a027b6864feec451f90af2189186
Soren Poole <qvu2hx@virgina.edu>
2025-11-29 13:12:02 -0500

    update

--- automesh_main.py
+++ automesh_main.py
@@ -9,12 +9,11 @@ from fluentfile_auto import CFD_job
 
 tot_case_folder = "C:/folder"
 
-
 case_lst = os.listdir(tot_case_folder)
 
 for case in case_lst:
     case_folder = case
     case_obj = CFD_job(case_folder)
     cad_path = [f for f in os.listdir(case) if f.lower().endswith(".step")][0]
-    case_obj._mesh_geometry(str(cad_path))
+    case_obj._mesh_geometry(cad_path)
 
